bf1f3a1637196060885904af1c63c0bdc5e29f3e
key <im_key@hotmail.com>
2019-01-20 01:39:09 +0800

    模块修改

--- 轮播图测试/js/slider.js
+++ 轮播图测试/js/slider.js
@@ -1,15 +1,21 @@
-function slider(){
-    var wrapper = document.querySelector('#list');
+function slide(obj){
+    
+    var parent = document.querySelector(obj.box);
+    var wrapper = parent.firstElementChild;
 // 轮播几张图片就使用图片总数+1的容器
 // 如果index等于1，说明是第一张图片，
 // 判断用户行为，如果点击左按钮，则首先无动画切到最后一张也就是代表其本身的图片
 // 再执行动画，此时画布会滑到真正的最后一张，
+var speed = obj.speed||50;
 var index = 0;
 var status= 0;
-var flag = true;
-var btnl = document.querySelector("#container .slide-btn-l");
-var btnr = document.querySelector("#container .slide-btn-r");
-var imgcount = document.querySelectorAll('#list a');
+
+var img = wrapper.querySelectorAll(obj.img);
+wrapper.appendChild(img[0].cloneNode(true));
+
+var btnl = document.querySelector(obj.btnl);
+var btnr = document.querySelector(obj.btnr);
+var imgcount = wrapper.querySelectorAll(obj.img);
 var imgWidth = imgcount[0].offsetWidth;
 var star = setInterval(play,3000);
 var starAnimate;
@@ -29,13 +35,13 @@ function play(){
 }
 
 function animate(arg){
-       var speed = flag? -50 : 50;
+    //    var speed = flag? -50 : 50;
        clearInterval(starAnimate);
        starAnimate = setInterval(go,100);
        function go(){
            var left = wrapper.offsetLeft;
            if(left > -(imgWidth)*index){
-               wrapper.style.left = left + speed + 'px';               
+               wrapper.style.left = left - speed + 'px';               
            }
        }
        console.log(imgWidth)
@@ -59,4 +65,6 @@ wrapper.onmouseout = function(){
 
 
 }
+
+export {slide};
 
