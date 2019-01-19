5188bec68289dd07d546676c5f58b40bcc4e095d
key <im_key@hotmail.com>
2019-01-19 20:42:51 +0800

    first commit

---
+++ 轮播图测试/js/index.js
@@ -0,0 +1,58 @@
+var wrapper = document.querySelector('#list');
+// 轮播几张图片就使用图片总数+1的容器
+// 如果index等于1，说明是第一张图片，
+// 判断用户行为，如果点击左按钮，则首先无动画切到最后一张也就是代表其本身的图片
+// 再执行动画，此时画布会滑到真正的最后一张，
+var index = 0;
+var status= 0;
+var flag = true;
+var btnl = document.querySelector("#container .slide-btn-l");
+var btnr = document.querySelector("#container .slide-btn-r");
+var imgcount = document.querySelectorAll('#list a');
+var imgWidth = imgcount[0].offsetWidth;
+var star = setInterval(play,3000);
+var starAnimate;
+
+
+function play(){
+    if(index ==imgcount.length-1){
+        clearInterval(star);
+        wrapper.style.left = 0;
+        index = 1;
+        star = setInterval(play,3000);
+    }else{
+       index++;
+       animate(index);
+    }
+                      
+}
+
+function animate(arg){
+       var speed = flag? -50 : 50;
+       clearInterval(starAnimate);
+       starAnimate = setInterval(go,100);
+       function go(){
+           var left = wrapper.offsetLeft;
+           if(left > -(imgWidth)*index){
+               wrapper.style.left = left + speed + 'px';               
+           }
+       }
+       console.log(imgWidth)
+     
+}
+
+wrapper.onmouseover = function(){
+    clearInterval(star);
+    btnl.onclick = function(){    
+        
+    }
+    
+    btnr.onclick = function(){
+        
+    }
+}
+
+wrapper.onmouseout = function(){
+    star = setInterval(play,3000)
+}
+
